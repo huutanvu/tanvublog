2895a3e65178bfda1729243d2673e57a7c1487da
Tan Vu <tanvu.nus@gmail.com>
2021-05-30 22:34:18 +0200

    Add some margin top to the navbar.

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="h-screen">
-      <div className="bg-white flex justify-center sticky z-20 top-0">
+      <div className="bg-white flex justify-center sticky z-20 top-0 mt-4">
         <div className="max-w-4xl w-screen">
-          <div className="flex justify-between items-center sm:m-8">
+          <div className="flex justify-between items-center m-8">
             <div>Logo</div>
             <nav className="flex space-x-4">
               <a href="#">Blog</a>
