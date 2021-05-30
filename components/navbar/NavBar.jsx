287007b744385a0459365782d7d9a51d5c223934
Tan Vu <tanvu.nus@gmail.com>
2021-05-30 22:21:06 +0200

    Simple NavBar

---
+++ components/navbar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="bg-white flex justify-center">
+      <div className="max-w-4xl w-screen">
+        <div className="flex justify-between items-center sm:m-8">
+          <div>Logo</div>
+          <nav className="flex space-x-4">
+            <a href="#">Blog</a>
+            <a href="#">About</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
