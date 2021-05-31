204e9f3c4e1c1cd10a963b5727b2773658d9fade
Tan Vu <tanvu.nus@gmail.com>
2021-05-31 20:25:06 +0200

    Update Container Layout

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="bg-white flex justify-center sticky z-20 top-0">
-      <div className="max-w-4xl w-screen pt-8">
+      <div className="py-8 w-full">
         <div className="flex justify-between items-center">
           <div>Tan Vu</div>
           <nav className="flex space-x-4">
